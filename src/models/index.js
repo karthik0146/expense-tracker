0152a871772c8e27c0fcc07439d25bd9ef7b668c
karthik0146 <byreddykartheek@gmail.com>
2025-11-06 12:25:22 +0530

    Delete src directory

--- src/models/index.js
+++
@@ -1,9 +0,0 @@
-const User = require('./user.model');
-const Category = require('./category.model');
-const Transaction = require('./transaction.model');
-
-module.exports = {
-    User,
-    Category,
-    Transaction
-};
